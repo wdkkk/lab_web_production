a2fef287ecd7a113b1b4f00e3bd97f6bec1ba0d7
matvey <fredddray@gmail.com>
2026-05-19 17:23:32 +0800

    fix: syntax tabulation

--- controllers/Router.php
+++ controllers/Router.php
@@ -1,83 +1,83 @@
-    <?php
+<?php
 
-    class Route
+class Route
+{
+    public string $route_regexp; // тут получается шаблона url
+    public $controller; // а это класс контроллера
+    public array $middlewareList = [];
+
+    public function middleware(BaseMiddleware $m): Route
     {
-        public string $route_regexp; // тут получается шаблона url
-        public $controller; // а это класс контроллера
-        public array $middlewareList = [];
-
-        public function middleware(BaseMiddleware $m): Route
-        {
-            array_push($this->middlewareList, $m);
-            return $this;
-        }
-
-        // ну и просто конструктор
-        public function __construct($route_regexp, $controller)
-        {
-            $this->route_regexp = $route_regexp;
-            $this->controller = $controller;
-        }
+        array_push($this->middlewareList, $m);
+        return $this;
     }
 
-    class Router
+    // ну и просто конструктор
+    public function __construct($route_regexp, $controller)
     {
-        /**
-         * @var Route[]
-         */
-        protected $routes = []; // создаем поле -- список под маршруты и привязанные к ним контроллеры
-
-        protected $twig;
-        protected $pdo;
-
-        // конструктор
-        public function __construct($twig, $pdo)
-        {
-            $this->twig = $twig;
-            $this->pdo = $pdo;
-        }
-
-        // функция с помощью которой добавляем маршрут
-        public function add($route_regexp, $controller)
-        {
-            $route = new Route($route_regexp, $controller);
-            array_push($this->routes, $route);
-
-            // возвращаем как результат функции
-            return $route;
-        }
-
-        public function get_or_default($default_controller)
-        {
-            $url = $_SERVER["REQUEST_URI"];
-
-            $path = parse_url($url, PHP_URL_PATH);
-
-            $controller = $default_controller;
-            $newRoute = null; // добавили переменную под маршрут
-
-            $matches = [];
-            foreach ($this->routes as $route) {
-                if (preg_match($route->route_regexp, $path, $matches)) {
-                    $controller = $route->controller;
-                    $newRoute = $route; // загоняем соответствующий url маршрут в переменную
-                    break;
-                }
-            }
-
-
-            $controllerInstance = new $controller();
-            $controllerInstance->setPDO($this->pdo);
-
-            if ($controllerInstance instanceof TwigBaseController) {
-                $controllerInstance->setTwig($this->twig);
-            }
-
-            if ($newRoute) {
-                foreach ($newRoute->middlewareList as $m) {
-                    $m->apply($controllerInstance, []);
-                }
-            }
-            return $controllerInstance->process_response();
-        }
+        $this->route_regexp = $route_regexp;
+        $this->controller = $controller;
     }
+}
+
+class Router
+{
+    /**
+     * @var Route[]
+     */
+    protected $routes = []; // создаем поле -- список под маршруты и привязанные к ним контроллеры
+
+    protected $twig;
+    protected $pdo;
+
+    // конструктор
+    public function __construct($twig, $pdo)
+    {
+        $this->twig = $twig;
+        $this->pdo = $pdo;
+    }
+
+    // функция с помощью которой добавляем маршрут
+    public function add($route_regexp, $controller)
+    {
+        $route = new Route($route_regexp, $controller);
+        array_push($this->routes, $route);
+
+        // возвращаем как результат функции
+        return $route;
+    }
+
+    public function get_or_default($default_controller)
+    {
+        $url = $_SERVER["REQUEST_URI"];
+
+        $path = parse_url($url, PHP_URL_PATH);
+
+        $controller = $default_controller;
+        $newRoute = null; // добавили переменную под маршрут
+
+        $matches = [];
+        foreach ($this->routes as $route) {
+            if (preg_match($route->route_regexp, $path, $matches)) {
+                $controller = $route->controller;
+                $newRoute = $route; // загоняем соответствующий url маршрут в переменную
+                break;
+            }
+        }
+
+
+        $controllerInstance = new $controller();
+        $controllerInstance->setPDO($this->pdo);
+
+        if ($controllerInstance instanceof TwigBaseController) {
+            $controllerInstance->setTwig($this->twig);
+        }
+
+        if ($newRoute) {
+            foreach ($newRoute->middlewareList as $m) {
+                $m->apply($controllerInstance, []);
+            }
+        }
+        return $controllerInstance->process_response();
+    }
+}
--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -1,40 +1,40 @@
-    <?php
-    require_once "TwigBaseController.php";
+<?php
+require_once "TwigBaseController.php";
 
-    class SearchController extends TwigBaseController
+class SearchController extends TwigBaseController
+{
+    public $template = "./pages/search.twig";
+
+    public function getContext(): array
     {
-        public $template = "./pages/search.twig";
+        $context = parent::getContext();
 
-        public function getContext(): array
-        {
-            $context = parent::getContext();
+        $age = $_GET['age'] ?? '';
+        $title = $_GET['title'] ?? '';
+        $description = $_GET['description'] ?? '';
 
-            $age = $_GET['age'] ?? '';
-            $title = $_GET['title'] ?? '';
-            $description = $_GET['description'] ?? '';
-
-            $sql = <<<EOL
+        $sql = <<<EOL
     SELECT title, short_name
     FROM cars_table
     WHERE (:title = '' OR title like CONCAT('%', :title, '%'))
     AND (:age = '' OR age = :age)
     EOL;
 
-            $query = $this->pdo->prepare($sql);
+        $query = $this->pdo->prepare($sql);
 
-            $query->bindValue("title", $title);
-            $query->bindValue("age", $age);
-            $query->bindValue("description", $description);
-            $query->execute();
+        $query->bindValue("title", $title);
+        $query->bindValue("age", $age);
+        $query->bindValue("description", $description);
+        $query->execute();
 
-            $context['search_params'] = [
-                "age" => $age,
-                "title" => $title,
-                "description" => $description
-            ];
+        $context['search_params'] = [
+            "age" => $age,
+            "title" => $title,
+            "description" => $description
+        ];
 
-            $context['objects'] = $query->fetchAll();
+        $context['objects'] = $query->fetchAll();
 
-            return $context;
-        }
+        return $context;
     }
+}
